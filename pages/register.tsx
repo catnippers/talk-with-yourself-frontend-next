5a574851a8ce286afe0a5b96e35fc726ec0f719b
gasparuss <kacperzielinski.primary@gmail.com>
2022-02-16 18:30:40 +0100

    Create Register Page

---
+++ pages/register.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '../components/Layout/Layout';
+import { RegisterForm } from '../components/RegisterForm/RegisterForm';
+
+const Register = () => {
+  return (
+    <Layout title="Register">
+      <RegisterForm />
+    </Layout>
+  );
+};
+
+export default Register;
