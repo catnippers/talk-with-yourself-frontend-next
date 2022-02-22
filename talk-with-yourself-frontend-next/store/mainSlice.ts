a4db61d46ee247f8404351771b1cd195530e70f6
Kacper Zieliński <86601708+Gasparuss@users.noreply.github.com>
2022-02-22 21:04:46 +0100

    Create Redux store

---
+++ talk-with-yourself-frontend-next/store/mainSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { UserRegisterData } from '../../types';
+
+export type InitialMainState = {
+  main: {
+    loading: boolean;
+    modal: { show: boolean; type: string; message: string };
+    isLogIn: boolean;
+    user: UserRegisterData;
+  };
+};
+
+export const slice = createSlice({
+  name: 'main',
+  initialState: {
+    loading: false,
+    modal: {
+      show: false,
+      type: '',
+      message: '',
+    },
+    isLogIn: false,
+    user: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  },
+  reducers: {
+    setLoading: (state, { payload }) => {
+      state.loading = payload;
+    },
+    showModal: (state, { payload }) => {
+      state.modal = {
+        show: true,
+        type: payload.type,
+        message: payload.message,
+      };
+    },
+    hideModal: (state) => {
+      state.modal = { ...state.modal, show: false };
+    },
+    setIsLogin: (state, { payload }) => {
+      state.isLogIn = payload;
+    },
+    setUser: (state, { payload }) => {
+      state.user = payload;
+    },
+  },
+});
+
+export const { setLoading, showModal, hideModal, setIsLogin, setUser } = slice.actions;
+
+export const getUser = (state: InitialMainState) => state.main.user;
+
+export const mainReducer = slice.reducer;
