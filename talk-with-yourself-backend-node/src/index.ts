82f152e507d21f17221b6883e883c814badf2801
Kacper Zieliński <86601708+Gasparuss@users.noreply.github.com>
2022-02-23 17:07:11 +0100

    install some packages

--- talk-with-yourself-backend-node/src/index.ts
+++ talk-with-yourself-backend-node/src/index.ts
@@ -2,13 +2,27 @@ import "dotenv-safe/config";
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { catchErrors, notFound } from "../src/middlewares/errors";
+import expressSession from "express-session";
+import { sessionConfig } from "./config/session";
 import users from "./routes/userRoutes";
+import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressSession(sessionConfig));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use("/api/users", users);
+
+app.use(notFound);
+app.use(catchErrors);
 
 app.listen(process.env.PORT);
