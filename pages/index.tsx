46507255b661989152101323206a0e46e29c47f8
Kacper Zieliński <kacperzielinski.primary@gmail.com>
2022-03-30 19:34:33 +0200

    Merge branch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,8 @@
-<<<<<<< HEAD
-import type { NextPage } from 'next';
-
-const Home: NextPage = () => {
-  return (
-    <div>
-      <h1>index</h1>
-    </div>
-  );
-=======
-<<<<<<< HEAD
-import { AuthChecker } from '../components/AuthChecker/AuthChecker';
-import { Layout } from '../components/Layout/Layout';
-
-const Home = () => {
-  return (
-    <AuthChecker>
-      <Layout>hej</Layout>
-    </AuthChecker>
-  );
-=======
 import Button from '@mui/material/Button';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return <Button variant="contained">Hello, world!</Button>;
->>>>>>> main
->>>>>>> catnippers-TWY-2
 };
 
 export default Home;
